7fd63cc34e760bc3a61f2aaa7fa1f22512f0277a
kevindarnet.work@gmail.com <kevindarnet.work@gmail.com>
2022-02-22 16:05:35 -0300

    .

--- src/Components/Cities.js
+++ src/Components/Cities.js
@@ -7,10 +7,6 @@ export default function Cities() {
       <div className="conteinerHeader">
         <div className="conteinertitlogo">
           <h1 className="tituloheader">Find your next adventure</h1>
-
-          <div className="buttoncitties">
-            <button className="btnhome">Search</button>
-          </div>
         </div>
       </div>
     </>
